f4a4be7e72c75da498a17339d14b92ff0b735862
Paul Salcido <psalcido@psalcido-desktop-dev.(none)>
2012-01-28 17:05:25 -0500

    Compiles with boost program options.

--- server/src/chessserver.cpp
+++ server/src/chessserver.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <boost/program_options.hpp>
 
 int main (int argc,char** argv) {
     std::cout << "Hello World." << std::endl;
+    boost::program_options::options_description desc("Allowed Options");
+    desc.add_options()
+        ("port",boost::program_options::value<int>(),"Port to open for primary connections.")
+        ("min-port-range",boost::program_options::value<int>(),"Ports to use for children connections.")
+        ("max-port-range",boost::program_options::value<int>(),"Ports to use for children connections.")
+    ;
+
+    boost::program_options::variables_map vm;
+    boost::program_options::store(
+        boost::program_options::parse_command_line(argc,argv,desc)
+        ,vm);
+    boost::program_options::notify(vm);
+
+    if ( vm.count("port") ) {
+        std::cout << "Port set to " << vm["port"].as<int>() << "." << std::endl;
+    }
+
+    return 0;
 }
