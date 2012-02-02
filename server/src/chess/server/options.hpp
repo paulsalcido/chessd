6b3dd8ad4518e97bb897664f84c6c4d43b68f32e
Paul Salcido <psalcido@psalcido-desktop-dev.(none)>
2012-02-01 21:51:14 -0500

    Moved configuration of options from command line to options.hpp.

--- server/src/chess/server/options.hpp
+++ server/src/chess/server/options.hpp
@@ -2,6 +2,9 @@
 #define CHESS_SERVER_OPTIONS_VERSION 0.1
 
 #include <string>
+#include <boost/program_options.hpp>
+#include <boost/lexical_cast.hpp>
+#include <chess/server/logger.hpp>
 
 namespace chess {
     namespace server {
@@ -52,6 +55,13 @@ namespace chess {
                 return this->m_log_directory;
             }
 
+            int option_errors(int i_option_errors) {
+                return this->m_option_errors = i_option_errors;
+            }
+            int option_errors() {
+                return this->m_option_errors;
+            }
+
             int port(int i_port) {
                 return this->m_port = i_port;
             }
@@ -59,10 +69,64 @@ namespace chess {
                 return this->m_port;
             }
 
+            int load_arguments(int argc, char **argv, chess::server::logger **logger) {
+                boost::program_options::options_description desc("Allowed Options");
+                desc.add_options()
+                    ("port",boost::program_options::value<int>(),"Port to open for primary connections.")
+                    ("min-port-range",boost::program_options::value<int>(),"Ports to use for children connections.")
+                    ("max-port-range",boost::program_options::value<int>(),"Ports to use for children connections.")
+                    ("log-directory",boost::program_options::value<std::string>(),"Logging directory.")
+                ;
+
+                int errors = 0;
+                boost::program_options::variables_map vm;
+                boost::program_options::store(
+                    boost::program_options::parse_command_line(argc,argv,desc)
+                    ,vm);
+                boost::program_options::notify(vm);
+                if ( vm.count("log-directory") ) {
+                    this->log_directory(vm["log-directory"].as<std::string>());
+                }
+            
+                if ( this->log_directory().length() > 0 ) {
+                    *logger = new chess::server::logger(this->log_directory());
+                } else {
+                    *logger = new chess::server::logger();
+                }
+            
+                (*logger)->log("Started logger");
+            
+                if ( vm.count("port") ) {
+                    this->port(vm["port"].as<int>());
+                    (*logger)->log("port retrieved " + boost::lexical_cast<std::string>(this->port()));
+                } else {
+                    (*logger)->error("Missing required option 'port'");
+                    errors++;
+                }
+            
+                if ( vm.count("min-port-range") ) {
+                    this->min_port_range(vm["min-port-range"].as<int>());
+                    (*logger)->log("Minimum port range set to " + boost::lexical_cast<std::string>(this->min_port_range()));
+                } else {
+                    (*logger)->error("Missing required option 'min-port-range'");
+                    errors++;
+                }
+            
+                if ( vm.count("max-port-range") ) {
+                    this->max_port_range(vm["max-port-range"].as<int>());
+                    (*logger)->log("Maximum port range set to " + boost::lexical_cast<std::string>(this->max_port_range()));
+                } else {
+                    (*logger)->error("Missing required option 'max-port-range'");
+                    errors++;
+                }
+                return this->option_errors(errors);
+            }
+
             protected:
             int m_port;
             int m_max_port_range;
             int m_min_port_range;
+            int m_option_errors;
             std::string m_log_directory;
         };
     }
--- server/src/chessserver.cpp
+++ server/src/chessserver.cpp
@@ -1,66 +1,10 @@
-#include <boost/program_options.hpp>
 #include <chess/server/options.hpp>
 #include <chess/server/logger.hpp>
-#include <boost/lexical_cast.hpp>
 
 int main (int argc,char** argv) {
-    boost::program_options::options_description desc("Allowed Options");
-    desc.add_options()
-        ("port",boost::program_options::value<int>(),"Port to open for primary connections.")
-        ("min-port-range",boost::program_options::value<int>(),"Ports to use for children connections.")
-        ("max-port-range",boost::program_options::value<int>(),"Ports to use for children connections.")
-        ("log-directory",boost::program_options::value<std::string>(),"Logging directory.")
-    ;
-
-    boost::program_options::variables_map vm;
-    boost::program_options::store(
-        boost::program_options::parse_command_line(argc,argv,desc)
-        ,vm);
-    boost::program_options::notify(vm);
-
-    boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
-    boost::gregorian::date today = now.date();
-
-    int errors = 0;
     chess::server::logger *logger;
-
     chess::server::options server_options;
+    server_options.load_arguments(argc,argv,&logger);
 
-    if ( vm.count("log-directory") ) {
-        server_options.log_directory(vm["log-directory"].as<std::string>());
-    }
-
-    if ( server_options.log_directory().length() > 0 ) {
-        logger = new chess::server::logger(server_options.log_directory());
-    } else {
-        logger = new chess::server::logger();
-    }
-
-    logger->log("Started logger");
-
-    if ( vm.count("port") ) {
-        server_options.port(vm["port"].as<int>());
-        logger->log("port retrieved " + boost::lexical_cast<std::string>(server_options.port()));
-    } else {
-        logger->error("Missing required option 'port'");
-        errors++;
-    }
-
-    if ( vm.count("min-port-range") ) {
-        server_options.min_port_range(vm["min-port-range"].as<int>());
-        logger->log("Minimum port range set to " + boost::lexical_cast<std::string>(server_options.min_port_range()));
-    } else {
-        logger->error("Missing required option 'min-port-range'");
-        errors++;
-    }
-
-    if ( vm.count("max-port-range") ) {
-        server_options.max_port_range(vm["max-port-range"].as<int>());
-        logger->log("Maximum port range set to " + boost::lexical_cast<std::string>(server_options.max_port_range()));
-    } else {
-        logger->error("Missing required option 'max-port-range'");
-        errors++;
-    }
-
-    return errors;
+    return server_options.option_errors();
 }
